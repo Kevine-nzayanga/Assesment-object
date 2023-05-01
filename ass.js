09fc5d27eb36e1740f10b1d10e006b3ae26956aa
student <kevinenzayanga@gmail.com>
2023-05-01 10:01:42 +0300

    add

--- ass.js
+++ ass.js
@@ -26,36 +26,64 @@ getAvailableBooks(books)
 
 // 2. Create a function getBooksByAuthor that takes an author's name as an argument and
 // returns an array of all books by that author.
-  let book =books.forEach(book =>
-    console.log(book))
+//   let book =books.forEach(book =>
+//    console.log(book))
 
-let bookAuthor=[]
-books.forEach(book =>{
-    bookAuthor.push( book.author)
-})
-console.log(bookAuthor);
 
-function getBooksByAuthor(bookAuthor) {
-   bookAuthor.forEach(title =>
-    console.log(title))
+// function getBooksByAuthor(author) {
+// this.
+//     books.forEach(element => {
+//         if (element.isAvailable==true) {
+//          newarr.push(element.title)
+//         }
 
- }
+//         function get(element) {
+//             let arr=[]
+//             for(element.author in element){
+//                arr.push (element)
+//             }
+//             console.log("arr", arr);
+//         }
+//         get(element)
+//      })
+
+//  console.log( bookAuthor.title); 
+
+//  }
+//  getBooksByAuthor(books)
+
+
+
+ const getBooksByAuthor=(name)=>{
+    let allBooksByAuthor = [];
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].author ==name) {
+            allBooksByAuthor.push(books[i])
+        }
+    }
+    return allBooksByAuthor;
+    }
+
+  console.log(  "yo",getBooksByAuthor("Charles Dickens"));
+
+
+
 
 // // 3. Create a function addNewBook that takes a book object as an argument and adds it
 // to the library, ensuring that the new book has all required properties (title, author,
 //     publicationYear, and isAvailable).
 
+let book =books.forEach(book =>
+    console.log(book))
 function addNewBook(books) {
-  
    console.log(books);
 }
-addNewBook("hey","gasb",2021)
+addNewBook(book)
 
 
 // 4. Create a function checkoutBook that takes a book title as an argument and changes
 // the book's isAvailable property to false. If the book is not found in the library, the
 // function should return a message indicating that the book is not available.
-
 
 function checkoutBook(books) {
     books.forEach(book =>
@@ -68,3 +96,8 @@ function checkoutBook(books) {
 function returnBook(books) {
     books.forEach(book)
 }
+
+
+
+
+  
